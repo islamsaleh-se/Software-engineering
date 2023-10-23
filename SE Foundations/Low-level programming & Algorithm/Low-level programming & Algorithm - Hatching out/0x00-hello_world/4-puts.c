66b7be2585fd03445b3291a41c8c43bcd2919681
ISlam <islamsaleh.se@gmail.com>
2023-10-23 14:42:49 +0300

    4. Hello, puts

---
+++ SE Foundations/Low-level programming & Algorithm/Low-level programming & Algorithm - Hatching out/0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+	* main - Entry point.
+	*
+	* Description: Prints string use the function puts.
+	*
+	* Return: Always 0 (Success).
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
